8a10954b31fe2f3e167ed6124c18fe3b563ae93e
Badu_Ser <100788930+badu-ser@users.noreply.github.com>
2025-10-21 22:26:19 +0530

    Update API URLs for category operations

--- frontend/src/redux/features/categoryApi.js
+++ frontend/src/redux/features/categoryApi.js
@@ -5,16 +5,16 @@ export const categoryApi = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     addCategory: builder.mutation({
       query: (data) => ({
-        url: "https://shofy-backend-dlt.vercel.app/api/category/add",
+        url: "https://shofy-electronics-ecommerce-ei29.onrender.com/api/category/add",
         method: "POST",
         body: data,
       }),
     }),
     getShowCategory: builder.query({
-      query: () => `https://shofy-backend-dlt.vercel.app/api/category/show`
+      query: () => `https://shofy-electronics-ecommerce-ei29.onrender.com/api/category/show`
     }),
     getProductTypeCategory: builder.query({
-      query: (type) => `https://shofy-backend-dlt.vercel.app/api/category/show/${type}`
+      query: (type) => `https://shofy-electronics-ecommerce-ei29.onrender.com/api/category/show/${type}`
     }),
   }),
 });
